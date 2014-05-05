5f03bf309d42c9969cbe1c427447d1048e853188
Alejandro Balbi <alejandrob@bandofcoders.com>
2014-05-05 10:44:17 -0300

    Demaciadas cosas todas juntas entre otras Card #9

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 var app = angular.module('proppApp', []);
 
-app.directive('contenteditable', function () {
+app.directive('contenteditable', function ($document,$window) {
   return {
     restrict: 'A', // only activate on element attribute
     require: '?ngModel', // get a hold of NgModelController
@@ -11,13 +11,20 @@ app.directive('contenteditable', function () {
         element.html(ngModel.$viewValue || '');
       };
 
-    // Listen for change events to enable binding
+
       element.on('blur change', function () {
-        scope.$apply(readViewText(scope));
+        scope.$apply(readViewText());
       });
 
       element.on('dblclick', function () {
         var sel = window.getSelection();
+//        console.log(sel)
+//        console.log(sel.getRangeAt(0))
+      });
+      element.on('keydown', function () {
+        var sel = window.getSelection();
+
+
 //        console.log(sel)
 //        console.log(sel.getRangeAt(0))
       });
@@ -26,45 +33,101 @@ app.directive('contenteditable', function () {
     // No need to initialize, AngularJS will initialize the text based on ng-model attribute
 
     // Write data to the model
-      function readViewText() {
+      function readViewText(evento) {
         var html;
-        html = scope.descripcion.render(element.text());
-        if (attrs.stripBr && html == '<br>') {
-          html = '';
+        var start = scope.descripcion.palabras.length;
+        var ultimo;
+        console.log(element);
+        sel = window.getSelection();
+        html = scope.descripcion.render(element.text(),$window);
+        if(evento) {
+          if (start != scope.descripcion.palabras.length) {
+            var id = '#'+scope.descripcion.palabras[scope.descripcion.palabras.length - 1].id;
+            scope.descripcion.ultimo = id;
+            scope.hecho.descripcion = html;
+          }
+        } else {
+          scope.hecho.descripcion = html;
         }
-        scope.hecho.descripcion = html;
       }
     }
   };
 });
   
+function getCaretPosition(editableDiv) {
+    var caretPos = 0, containerEl = null, sel, range;
+    if (window.getSelection) {
+        sel = window.getSelection();
+        if (sel.rangeCount) {
+            range = sel.getRangeAt(0);
+            console.log(range)
+            //if (range.commonAncestorContainer.parentNode == editableDiv) {
+                caretPos = range.endOffset;
+            //}
+        }
+    } else if (document.selection && document.selection.createRange) {
+        range = document.selection.createRange();
+        if (range.parentElement() == editableDiv) {
+            var tempEl = document.createElement("span");
+            editableDiv.insertBefore(tempEl, editableDiv.firstChild);
+            var tempRange = range.duplicate();
+            tempRange.moveToElementText(tempEl);
+            tempRange.setEndPoint("EndToEnd", range);
+            caretPos = tempRange.text.length;
+        }
+    }
+    return caretPos;
+}
+
+
 app.controller('hechoCtrl', function($scope) {
-  $scope.hecho = {descripcion: 'Lorem ipsum ad his scripta blandit partiendo, eum fastidii accumsan euripidis in, eum liber hendrerit an. Qui ut wisi vocibus suscipiantur, quo dicit ridens inciderint id. Quo mundi lobortis reformidans eu, legimus senserit definiebas an eos. Eu sit tincidunt incorrupte definitionem, vis mutat affert percipit cu, eirmod consectetuer signiferumque eu per. In usu latine equidem dolores. Quo no falli viris intellegam, ut fugit veritus placerat per.'};
+  $scope.hecho = {descripcion: 'Lorem de Ipsum Ad his scripta blandit partiendo, eum fastidii accumsan euripidis in, eum liber hendrerit an. Qui ut wisi vocibus suscipiantur, quo dicit ridens inciderint id. Quo mundi lobortis reformidans eu, legimus senserit definiebas an eos. Eu sit tincidunt incorrupte definitionem, vis mutat affert percipit cu, eirmod consectetuer signiferumque eu per. In usu latine equidem dolores. Quo no falli viris intellegam, ut fugit veritus placerat per.'};
+  $scope.elementos = {
+    ivana: {nombre: 'Ivana',etiquetas: []}
+  }
   $scope.etiquetas = {
-    quo: {},
-    liber: {}
+    toreador: {
+      items: []
+    }
+  }
+  $scope.elementos.ivana.etiquetas.push('toreador');
+  $scope.etiquetas.toreador.items.push($scope.elementos.ivana);
+  $scope.elemento = {
+    item: {},
+    visible: false,
+    agregar: function() {
+      $scope.elementos[$scope.elemento.item.nombre.toLowerCase().trim()] = {nombre: $scope.elemento.item.nombre.trim()}
+      $scope.elemento.visible = false;
+      $scope.elemento.item = {};
+    }
   }
   $scope.descripcion = {
     palabras: [],
-    render: function(text) {
+    render: function(text, $window) {
       html = '';
       $scope.descripcion.palabras = [];
+      text = text.trim();
       text = text.replace(/\&nbsp\;/gi," ");
-      text = text.replace(/\w+|./mig, function(match){
+      text = text.replace(/(\w+)|(.)/mg, function(match){
         var obj = {}
         obj.texto = match;
-        if($scope.etiquetas[match.toLowerCase()]) {
+        obj.id = 'palabra-' + $scope.descripcion.palabras.length
+        if($scope.etiquetas[match.toLowerCase().trim()]) {
           obj.etiqueta = true;
-          match = '<span class="etiqueta">' + match +'</span>'
+          match = '<span id="' + obj.id + '" class="etiqueta">' + match +'</span>'
+          $scope.descripcion.palabras.push(obj)
+        } else if($scope.elementos[match.toLowerCase().trim()]) {
+          obj.elemento = true;
+          match = '<span id="' + obj.id + '" class="elemento">' + match +'</span>'
+          $scope.descripcion.palabras.push(obj)
         } else {
-          match = '<span>' + match +'</span>'
+          //match = '<span>' + match +'</span>'
         }
         html = html + match;
-        $scope.descripcion.palabras.push(obj)
       });
+      html = html + '<span> </span>'
       return html;
     }
   }
   $scope.hecho.descripcion = $scope.descripcion.render($scope.hecho.descripcion);
 });
-
